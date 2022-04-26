1b89fd80f8c0646b9e91d073c5f523eafcf5c139
Nikita Rehemae <nikita.rehemae@gmail.com>
2022-04-26 18:38:04 +0300

    Add CompoundComponent to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Expandable from "./components/Expandable";
+import './App.styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CompoundComponent = () => {
+    return (
+        <Expandable>
+            <Expandable.Header>React Hooks</Expandable.Header>
+            <Expandable.Icon />
+            <Expandable.Body>Hooks are awesome</Expandable.Body>
+        </Expandable>
+    );
+}
+
+function App () {
+    return (
+        <div className="App">
+            <CompoundComponent/>
+        </div>
+    );
 }
 
 export default App;
